f0afcdb4e9166cdcef4bd319be071e3ce63054cb
batomow <carlosa_miranda@outlook.com>
2020-04-04 11:19:46 -0700

    Adds BOILERPLATE for Server and Cloud Firestore
    I've also included some test reads and writes commented

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const admin = require('firebase-admin')
-const serviceAccount = require('./ServiceAccountKey')
+//init express
 const express = require('express')
 const app = express()
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
-const db = admin.firestore()
-getQuote().then(result=>{
-    console.log()
-})
-
 const PORT = process.env.PORT | 8000
 
 app.get('/', (req, res)=>{'Arquidiocesis Backend'})
 
 app.listen(PORT, ()=>{console.log(`Listening on port: ${PORT}...`)})
+
+//init firebase
+const admin = require('firebase-admin')
+const serviceAccount = require('./ServiceAccountKey')
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+const db = admin.firestore()
+
+// TEST WRITE
+// let aTuringRef = db.collection('users').doc('aturing');
+
+// let setAlan = aTuringRef.set({
+//   'first': 'Alan',
+//   'middle': 'Mathison',
+//   'last': 'Turing',
+//   'born': 1912
+// });
+
+// TEST READ
+// db.collection('users').get()
+//     .then((snapshot)=>{
+//         snapshot.forEach((doc)=>{
+//             console.log(doc.id, '=>', doc.data())
+//         })
+//     })
+//     .catch((err)=>{
+//         console.log('Error getting documents', err)
+//     })
