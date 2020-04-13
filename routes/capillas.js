03a6d27acfb6839d8d1dc48bca0045ddb37e7a62
batomow <carlosa_miranda@outlook.com>
2020-04-12 20:49:32 -0700

    Adds add capilla
    It cascades and updates the parroquia,
    remember to cascade for delete!

--- routes/capillas.js
+++ routes/capillas.js
@@ -7,19 +7,39 @@ const add = async (firestore, req, res)=>{
         parroquia: req.body.parroquia
     }
     const collectionref = await firestore.collection('capillas')
-    try{
-        const docref = await collectionref.add(newCapilla)
-        res.send({
-            error: false, 
-            id: docref.id
-        })
-        // update parroquia 
-    } catch(err){
-        res.send({
+    const parroquiaref = await firestore.collection('parroquias').doc(req.body.parroquia)
+    //validate parroquia 
+    const snapshot = await parroquiaref.get()
+    if (!snapshot.exists){
+        return res.send({
             error: true, 
-            message: err.message
+            message: 'couldn\'t find parroquia with he given id'
         })
     }
+    //--- add self to parroquia  --- // 
+    // -----------VVVVVVV----------- //
+
+    let capillas = snapshot.data().capillas //find stored data 
+    const docref = await collectionref.add(newCapilla) // add new capilla to capillas collection
+    if (capillas) {capillas.push(docref.id)} // create new array if it doesn't exists
+    else { capillas = [docref.id] }
+        
+    //rewrite with new data in parroquias
+    try{ await parroquiaref.set({capillas: capillas}, {merge: true})
+    } catch(err){
+        return res.send({
+            error: true, 
+            message: 'unexpected error while adding new capilla to capillas list in parroquia',
+            details: err.message
+        })
+    }
+   
+    // --------- success ----------//
+    // ----------VVVVVVV-----------//
+    res.send({
+        error: false, 
+        id: docref.id
+    })
 }
 
 module.exports = {
