0a380893ebffa45ac0e95adc194165fe2cf6ef0d
Diego Martinez <diegomtz1998@hotmail.com>
2020-11-27 13:09:16 -0600

    fix(capacitacion dump): fix capacitacion report

--- routes/capacitacion.js
+++ routes/capacitacion.js
@@ -669,36 +669,45 @@ const getAsistenciasAsistanceReport = async (firestore, req, res)=>{
 var dump = async (firestore, req, res)=>{
     try{
         var capSnap = await firestore.collection('capacitaciones').get();
-
-        var coordIds = [...new Set(capSnap.docs.map(a=>a.data().encargado).filter(a=>(a ? true : false)))];
-        var coordinadores = []
-        if(coordIds.length>0){
-            var coordSnap = await firestore.getAll(...coordIds.map(a=>firestore.doc('coordinadores/'+a)))
-            coordSnap.forEach(a=>{
-                if(!a.exists) return;
-                coordinadores.push({
-                    id: a.id,
-                    ...a.data()
-                });
-            })
-        }  
+				
+				var capacitadores = [];
+				const loginSnap = await firestore.collection('logins').where('tipo', '==', 'capacitacion').get();
+				for (l of loginSnap.docs) {
+					const info = await firestore.collection('admins').doc(l.data().id).get();
+					if (!info.data().apellido_materno) {
+						capacitadores.push({
+							id: l.data().id,
+							email: l.id,
+							apellido_materno: '',
+							...info.data()
+						})
+					} else {
+						capacitadores.push({
+							id: l.data().id,
+							email: l.id,
+							...info.data()
+						})
+					}
+					
+				}
 
         var capacitaciones = []
         capSnap.docs.forEach(a=>{
             if(!a.exists) return;
             var d = a.data();
-            var coord = coordinadores.find(a=>a.id==d.encargado);
+            var cap = capacitadores.find(a=>a.id==d.encargado);
             capacitaciones.push([
                 a.id,
                 d.nombre,
-                coord.id,
-				`${coord.nombre} ${coord.apellido_paterno} ${coord.apellido_materno}`,
+                cap.id,
+				`${cap.nombre} ${cap.apellido_paterno} ${cap.apellido_materno}`,
+				cap.email,
 				(d.inicio && d.inicio._seconds) ? moment.unix(d.inicio._seconds).format('YYYY-MM-DD') : '',
 				(d.fin && d.fin._seconds) ? moment.unix(d.fin._seconds).format('YYYY-MM-DD') : ''
             ]);
         });
 
-        var headers = [ 'IDCapacitacion', 'Nombre', 'IDCoordinador', 'Coordinador', 'Fecha inicio', 'Fech fin' ];
+        var headers = [ 'IDCapacitacion', 'Nombre', 'IDCoordinador', 'Coordinador', 'Email', 'Fecha inicio', 'Fech fin' ];
         var csv = Util.toXLS(headers, capacitaciones);
 
         res.setHeader('Content-Type', 'application/vnd.ms-excel');
