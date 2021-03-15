de9ae47c4d2dfcf2ef544d4cedaa663e34e8f607
Daniel Gaytan <dnigaytan07@gmail.com>
2021-03-15 03:29:22 -0600

    Solved lint warnings and problems.

--- __tests__/grupo-conv.spec.js
+++ __tests__/grupo-conv.spec.js
@@ -28,53 +28,53 @@ mockFirebase({
 
 //Creating test suite for grupo-conv
 describe('Testing "Grupo conversacion"', () => {
-  const admin = require('firebase-admin')
-    const db = admin.firestore()
-    test('Testing correct "add" functionality', async () => {
-        const request = mockRequest(
-            {
-              'nombre' : 'testing-input-1',
-              'canales': [],
-              'roles': []
-            }
-        )
-        const res = mockResponse()
-        await grupo.add(db, request, res)
-        expect(res.status).toHaveBeenCalledWith(200)
-    });
+  const admin = require('firebase-admin');
+  const db = admin.firestore();
+  test('Testing correct "add" functionality', async () => {
+    const request = mockRequest(
+      {
+        'nombre' : 'testing-input-1',
+        'canales': [],
+        'roles': []
+      }
+    );
+    const res = mockResponse();
+    await grupo.add(db, request, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
 
-    test('Testing incorrect (canales not in db) "add" functionality', async () => {
-      const request = mockRequest(
-          {
-            'nombre' : 'testing-input-1',
-            'canales': ['d1'],
-            'roles': []
-          }
-      )
-      const res = mockResponse()
-      await grupo.add(db, request, res)
-      expect(res.send).toHaveBeenCalledWith({
-        error: true,
-        message: "couldn't find canal with the given id",
-     })
+  test('Testing incorrect (canales not in db) "add" functionality', async () => {
+    const request = mockRequest(
+      {
+        'nombre' : 'testing-input-1',
+        'canales': ['d1'],
+        'roles': []
+      }
+    );
+    const res = mockResponse();
+    await grupo.add(db, request, res);
+    expect(res.send).toHaveBeenCalledWith({
+      error: true,
+      message: "couldn't find canal with the given id",
     });
+  });
 
-    test('Testing incorrect (roles not in db) "add" functionality', async () => {
-      const request = mockRequest(
-          {
-            'nombre' : 'testing-input-1',
-            'canales': [],
-            'roles': ['id1']
-          }
-      )
-      const res = mockResponse()
-      await grupo.add(db, request, res)
-      expect(res.send).toHaveBeenCalledWith({
-        error: true,
-        message: "couldn't find role with the given id",
-     })
+  test('Testing incorrect (roles not in db) "add" functionality', async () => {
+    const request = mockRequest(
+      {
+        'nombre' : 'testing-input-1',
+        'canales': [],
+        'roles': ['id1']
+      }
+    );
+    const res = mockResponse();
+    await grupo.add(db, request, res);
+    expect(res.send).toHaveBeenCalledWith({
+      error: true,
+      message: "couldn't find role with the given id",
     });
+  });
 });
 
-jest.clearAllMocks()
-mockCollection.mockClear()
+jest.clearAllMocks();
+mockCollection.mockClear();
--- routes/grupo-conv.js
+++ routes/grupo-conv.js
@@ -4,60 +4,60 @@ const Util = require('./util');
  * @module Grupo-conv
  */
 
- const add = async (firestore, req, res) => {
-     let {
-         nombre,
-         roles,
-         canales,
-     } = req.body
+const add = async (firestore, req, res) => {
+  const {
+    nombre,
+    roles,
+    canales,
+  } = req.body;
 
-     let snapshot_roles = []
+  const snapshot_roles = [];
 
-     // Checks if all roles exist within collection 'roles'
-     for(role of roles){
-        const roleref = await firestore.collection('roles').doc(role)
-        //validate role
-        const snapshot = await roleref.get()
-        if (!snapshot.exists){
-            return res.send({
-                error: true,
-                message: 'couldn\'t find role with the given id'
-            })
-        }
-        snapshot_roles.add(snapshot.id)
-     }
+  // Checks if all roles exist within collection 'roles'
+  for(role of roles){
+    const roleref = await firestore.collection('roles').doc(role);
+    //validate role
+    const snapshot = await roleref.get();
+    if (!snapshot.exists){
+      return res.send({
+        error: true,
+        message: 'couldn\'t find role with the given id'
+      });
+    }
+    snapshot_roles.add(snapshot.id);
+  }
 
-     let snapshot_canales = []
+  const snapshot_canales = [];
 
-     // Checks if all canales exist within collection 'canales'
-     for(canal of canales){
-        const canalref = await firestore.collection('canales').doc(canales)
-        //validate canal
-        const snapshot = await canalref.get()
-        if (!snapshot.exists){
-            return res.send({
-                error: true,
-                message: 'couldn\'t find canal with the given id'
-            })
-        }
-        snapshot_canales.add(snapshot.id)
-     }
+  // Checks if all canales exist within collection 'canales'
+  for(canal of canales){
+    const canalref = await firestore.collection('canales').doc(canales);
+    //validate canal
+    const snapshot = await canalref.get();
+    if (!snapshot.exists){
+      return res.send({
+        error: true,
+        message: 'couldn\'t find canal with the given id'
+      });
+    }
+    snapshot_canales.add(snapshot.id);
+  }
 
-     const collectionref = await firestore.collection('grupo-conv')
-     const docref = await collectionref.add({
-        nombre,
-        roles: snapshot_roles,
-        canales: snapshot_canales
-     }) // add new grupo-conv to grupo-conv collection
+  const collectionref = await firestore.collection('grupo-conv');
+  const docref = await collectionref.add({
+    nombre,
+    roles: snapshot_roles,
+    canales: snapshot_canales
+  }); // add new grupo-conv to grupo-conv collection
    
-     // --------- success ----------//
-     // ----------VVVVVVV-----------//
-     res.status(200).send({
-        error: false,
-        data: docref.id
-     })
- }
+  // --------- success ----------//
+  // ----------VVVVVVV-----------//
+  res.status(200).send({
+    error: false,
+    data: docref.id
+  });
+};
 
- module.exports = {
-     add: add
- }
+module.exports = {
+  add: add
+};
