3f3adbc921830b3794e20fa18d37288d3fb986a0
Daniel Gaytan <dnigaytan07@gmail.com>
2021-04-16 20:17:08 -0500

    Modified post or publicacion. -Entity is already embedded in backend code. -Add, get, edit, get_files functionalities are in backend code. -Test suite for post is working properly... every function has its own test.

--- routes/publicacion.js
+++ routes/publicacion.js
@@ -15,7 +15,14 @@ post-comments: ['comment-id-1', 'comment-id-2', 'comment-id-3'],
 */
 
 const add = async (firestore, req, res) => {
-  const { post_text, post_author, post_files } = req.body;
+  const { post_text, post_author, post_files, channel_owner_id } = req.body;
+
+  if (channel_owner_id === '' || channel_owner_id === undefined) {
+    return res.send({
+      error: true,
+      message: 'Channel owner ID should not be left blank',
+    });
+  }
 
   if (
     post_text === '' ||
@@ -29,37 +36,76 @@ const add = async (firestore, req, res) => {
     });
   }
 
-  const collectionref = await firestore.collection('publicacion');
-  const docref = await collectionref.add({
-    post_text,
-    post_author,
-    post_files,
-  }); // add new publicacion to publicacion collection
+  try {
+    const collectionref = await firestore.collection('publicacion');
+    const docref = await collectionref.add({
+      post_author,
+      post_text,
+      post_files,
+    }); // add new publicacion to publicacion collection
 
-  // --------- success ----------//
-  // ----------VVVVVVV-----------//
-  res.send({
-    error: false,
-    data: docref.id,
-  });
+    res.send({
+      error: false,
+      data: docref.id,
+    });
+  } catch (e) {
+    res.send({
+      error: true,
+      message: `Unexpected error: ${e}`,
+    });
+  }
 };
 
 const edit = async (firestore, req, res) => {
   const { post_id, post_text, post_files } = req.body;
 
-  await firestore.collection('publicacion').doc(post_id).update({
-    post_text,
-    post_files,
-  });
+  try {
+    await firestore.collection('publicacion').doc(post_id).update({
+      post_text,
+      post_files,
+    });
 
-  // --------- success ----------//
-  // ----------VVVVVVV-----------//
-  return res.send({
-    error: false,
-  });
+    return res.send({
+      error: false,
+    });
+  } catch (e) {
+    res.send({
+      error: true,
+      message: `Unexpected error: ${e}`,
+    });
+  }
 };
 
-const get_files = async (firestore, req, res) => {
+const get = async (firestore, req, res) => {
+  const { id } = req.params; //post ID
+
+  try {
+    const postRef = await firestore.collection('publicacion').doc(id);
+    const post = await postRef.get();
+    if (!post.empty) {
+      return res.send({
+        error: false,
+        data: {
+          post: {
+            id: post.id,
+            ...post.data(),
+          },
+        },
+      });
+    }
+    return res.send({
+      error: true,
+      message: `No post with ID: ${id}`,
+    });
+  } catch (e) {
+    res.send({
+      error: true,
+      message: `Unexpected error: ${e}`,
+    });
+  }
+};
+
+const get_post_files = async (firestore, req, res) => {
   const { post_id } = req.body;
 
   await firestore
@@ -79,5 +125,6 @@ const get_files = async (firestore, req, res) => {
 module.exports = {
   add: add,
   edit: edit,
-  retrieve_post_files: get_files,
+  get: get,
+  get_post_files: get_post_files,
 };
