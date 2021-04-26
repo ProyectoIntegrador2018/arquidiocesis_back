b4ec4632e1a93e931d6e092029116999634af2f9
Cesar Barraza <cbarrazaa1@gmail.com>
2021-04-26 13:59:53 -0500

    test(post-comments): modify get post comments test to use params instead of body

--- __tests__/comentario.spec.js
+++ __tests__/comentario.spec.js
@@ -1,21 +1,18 @@
-const {
-  mockCollection,
-  mockDoc,
-} = require('firestore-jest-mock/mocks/firestore');
+const { mockCollection } = require('firestore-jest-mock/mocks/firestore');
 const { mockFirebase } = require('firestore-jest-mock');
 const comentario = require('../routes/comentario.js');
-  
+
 const mockRequest = (body, params) => ({
   body,
   params,
 });
-  
+
 const mockResponse = () => {
   const res = {};
   res.send = jest.fn().mockReturnValue(res);
   return res;
 };
-  
+
 //Creating fake firebase database with comentario collection only
 mockFirebase({
   database: {
@@ -35,11 +32,11 @@ mockFirebase({
     ],
   },
 });
-  
+
 describe('Comentario functionalities test suite', () => {
   const admin = require('firebase-admin');
   const db = admin.firestore();
-  
+
   test('Testing correct add functionality', async () => {
     const req = mockRequest({
       comment_text: 'dummy comentario text',
@@ -47,24 +44,24 @@ describe('Comentario functionalities test suite', () => {
       post_owner_id: '1',
     });
     const res = mockResponse();
-  
+
     await comentario.add(db, req, res);
-  
+
     expect(mockCollection).toHaveBeenCalledWith('comentario');
     expect(res.send).toHaveBeenCalledWith(
       expect.objectContaining({ error: false })
     );
   });
-  
+
   test('Testing incorrect add functionality: field in blank', async () => {
     const req = mockRequest({
       comment_text: 'dummy comment text',
-      post_owner_id:'1',
+      post_owner_id: '1',
     });
     const res = mockResponse();
-  
+
     await comentario.add(db, req, res);
-  
+
     expect(mockCollection).toHaveBeenCalledWith('comentario');
     expect(res.send).toHaveBeenCalledWith(
       expect.objectContaining({
@@ -73,16 +70,16 @@ describe('Comentario functionalities test suite', () => {
       })
     );
   });
-  
+
   test('Testing incorrect add functionality: no post owner id', async () => {
     const req = mockRequest({
       comment_text: 'dummy comment text',
       comment_author: '2',
     });
     const res = mockResponse();
-  
+
     await comentario.add(db, req, res);
-  
+
     expect(mockCollection).toHaveBeenCalledWith('comentario');
     expect(res.send).toHaveBeenCalledWith(
       expect.objectContaining({
@@ -91,17 +88,16 @@ describe('Comentario functionalities test suite', () => {
       })
     );
   });
-  
+
   test('Testing correct getPostComments functionality', async () => {
-    const req = mockRequest({post_owner_id:'1'}, {});
+    const req = mockRequest({}, { postID: '1' });
     const res = mockResponse();
-  
+
     await comentario.getPostComments(db, req, res);
-  
+
     expect(mockCollection).toHaveBeenCalledWith('comentario');
     expect(res.send).toHaveBeenCalledWith(
       expect.objectContaining({ error: false })
     );
   });
 });
-  
